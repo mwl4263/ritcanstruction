7f6af10c2a4396455dd46ccde8b944376505c20d
Mason Lapine <60404755+mwl4263@users.noreply.github.com>
2023-09-14 00:06:02 -0400

    resized images

--- src/assets/2018/index.js
+++ src/assets/2018/index.js
@@ -1,65 +1,65 @@
-import img1 from "./IMG_4177.JPG";
-import img2 from "./IMG_4179.JPG";
-import img3 from "./IMG_4186.JPG";
-import img4 from "./IMG_4189.JPG";
-import img5 from "./IMG_4190.JPG";
-import img6 from "./IMG_4191.JPG";
-import img7 from "./IMG_4192.JPG";
-import img8 from "./IMG_4193.JPG";
-import img9 from "./IMG_4194.JPG";
-import img10 from "./IMG_4195.JPG";
-import img11 from "./IMG_4196.JPG";
-import img12 from "./IMG_4197.JPG";
-import img13 from "./IMG_4198.JPG";
-import img14 from "./IMG_4199.JPG";
-import img15 from "./IMG_4200.JPG";
-import img16 from "./IMG_4201.JPG";
-import img17 from "./IMG_4202.JPG";
+import img1 from "./IMG_4177.jpg";
+import img2 from "./IMG_4179.jpg";
+import img3 from "./IMG_4186.jpg";
+import img4 from "./IMG_4189.jpg";
+import img5 from "./IMG_4190.jpg";
+import img6 from "./IMG_4191.jpg";
+import img7 from "./IMG_4192.jpg";
+import img8 from "./IMG_4193.jpg";
+import img9 from "./IMG_4194.jpg";
+import img10 from "./IMG_4195.jpg";
+import img11 from "./IMG_4196.jpg";
+import img12 from "./IMG_4197.jpg";
+import img13 from "./IMG_4198.jpg";
+import img14 from "./IMG_4199.jpg";
+import img15 from "./IMG_4200.jpg";
+import img16 from "./IMG_4201.jpg";
+import img17 from "./IMG_4202.jpg";
 import img18 from "./IMG_4203.JPG";
-import img19 from "./IMG_4204.JPG";
-import img20 from "./IMG_4205.JPG";
-import img21 from "./IMG_4206.JPG";
-import img22 from "./IMG_4207.JPG";
-import img23 from "./IMG_4208.JPG";
-import img24 from "./IMG_4209.JPG";
-import img25 from "./IMG_4210.JPG";
-import img26 from "./IMG_4211.JPG";
-import img27 from "./IMG_4212.JPG";
-import img28 from "./IMG_4213.JPG";
-import img29 from "./IMG_4214.JPG";
-import img30 from "./IMG_4219.JPG";
-import img31 from "./IMG_4220.JPG";
+import img19 from "./IMG_4204.jpg";
+import img20 from "./IMG_4205.jpg";
+import img21 from "./IMG_4206.jpg";
+import img22 from "./IMG_4207.jpg";
+import img23 from "./IMG_4208.jpg";
+import img24 from "./IMG_4209.jpg";
+import img25 from "./IMG_4210.jpg";
+import img26 from "./IMG_4211.jpg";
+import img27 from "./IMG_4212.jpg";
+import img28 from "./IMG_4213.jpg";
+import img29 from "./IMG_4214.jpg";
+import img30 from "./IMG_4219.jpg";
+import img31 from "./IMG_4220.jpg";
 
 export {
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-  img12,
-  img13,
-  img14,
-  img15,
-  img16,
-  img17,
-  img18,
-  img19,
-  img20,
-  img21,
-  img22,
-  img23,
-  img24,
-  img25,
-  img26,
-  img27,
-  img28,
-  img29,
-  img30,
-  img31,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
+    img23,
+    img24,
+    img25,
+    img26,
+    img27,
+    img28,
+    img29,
+    img30,
+    img31,
 };
--- src/assets/2019/index.js
+++ src/assets/2019/index.js
@@ -1,17 +1,17 @@
 import img1 from "./BB-8.jpg";
-import img2 from "./IMG_0688.jpeg";
-import img3 from "./IMG_0689.jpeg";
+import img2 from "./IMG_0688.jpg";
+import img3 from "./IMG_0689.jpg";
 import img4 from "./IMG_0692.PNG";
-import img5 from './IMG_0693.jpeg';
-import img6 from './IMG_0694.jpeg';
-import img7 from './IMG_0698.jpeg';
-import img8 from './IMG_0700.jpeg';
-import img9 from './IMG_0701.jpeg';
-import img10 from './IMG_0702.jpeg';
-import img11 from './IMG_0707.jpeg';
-import img12 from './IMG_0708.jpeg';
-import img13 from './IMG_0709.jpeg';
-import img14 from './IMG_0710.jpeg';
+import img5 from './IMG_0693.jpg';
+import img6 from './IMG_0694.jpg';
+import img7 from './IMG_0698.jpg';
+import img8 from './IMG_0700.jpg';
+import img9 from './IMG_0701.jpg';
+import img10 from './IMG_0702.jpg';
+import img11 from './IMG_0707.jpg';
+import img12 from './IMG_0708.jpg';
+import img13 from './IMG_0709.jpg';
+import img14 from './IMG_0710.jpg';
 
 export {
     img14,
